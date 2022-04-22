0e8a104305a944b18f6ab24423dc936a31832f33
Christina Kalb <kalb@ucar.edu>
2022-04-22 14:15:12 -0600

    Update plot_station_bias.py

--- metplotpy/contributed/point_stat/plot_station_bias.py
+++ metplotpy/contributed/point_stat/plot_station_bias.py
@@ -1,1 +1,18 @@
+# ============================*
+ # ** Copyright UCAR (c) 2020
+ # ** University Corporation for Atmospheric Research (UCAR)
+ # ** National Center for Atmospheric Research (NCAR)
+ # ** Research Applications Lab (RAL)
+ # ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+ # ============================*
+ 
+ 
+ 
+"""
+Plotting a station bias plot from point stat
+ """
 
+import os
+from matplotlib import pyplot as plt
+import yaml
+from metplotpy.plots import util
